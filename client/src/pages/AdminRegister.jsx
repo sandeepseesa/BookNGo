c26697e43917b26e17dc89a81c25968a4b8e6c5a
sandeepseesa <sandeepseesa@gmail.com>
2024-12-20 16:09:59 +0530

    change

--- client/src/pages/AdminRegister.jsx
+++ client/src/pages/AdminRegister.jsx
@@ -54,7 +54,7 @@ function AdminRegister() {
                     </div>
 
                     <div>
-                    <h2 class="text-2xl font-bold text-gray-800 text-center">Admin Registration</h2>
+                    <h2 className="text-2xl font-bold text-gray-800 text-center">Admin Registration</h2>
                         {/* <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                             Admin Registration
                         </h2> */}
